6f434da492bb8ec2b827369a9ebae0224dada591
peterpans2013 <31445699+peterpans2013@users.noreply.github.com>
2021-05-29 20:33:16 +0700

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,3 @@
-sudo python -m pip install torch==0.4.1.post2 torchvision==0.2.1 PyQt5==5.12
+sudo python3 -m pip install torch==0.4.1.post2 torchvision==0.2.1 PyQt5==5.12
 
 sudo apt install imagemagick
